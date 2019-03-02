78845f79d3fad95e5e70d9b3dd5bd9421a92d256
thankcreate <thankcreate@gmail.com>
2019-03-01 21:43:15 -0500

    week 5

---
+++ Week5/Assets/Scenes/Assignment/PerlinMeshWriter.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class PerlinMeshWriter : MonoBehaviour {
+
+    #region vars
+    MeshFilter mf;
+    MeshRenderer mr;
+
+    Mesh mesh;
+    Vector3[] vertices;
+    Vector3[] normals;  
+    int[] tris;
+    Vector2[] uvs;
+    int gridPerMeshSide = 8;
+    public float heightFactor = 2;
+    Vector3 lastPosi;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        
+        mf = GetComponent<MeshFilter>();
+        mr = GetComponent<MeshRenderer>();
+
+        Init();
+        UpdateMesh(true);
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateMesh();
+    }
+
+    void UpdateMesh(bool forceUpdate = false)
+    {       
+        if(forceUpdate || transform.position != lastPosi)
+        {
+            UpdateMeshInner(forceUpdate);
+            ApplyMeshData();
+        }
+        lastPosi = transform.position;
+    }
+
+    
+    
+    void Init()
+    {        
+        var verticesCount = (gridPerMeshSide + 1) * (gridPerMeshSide + 1);
+
+        mesh = new Mesh();
+        vertices = new Vector3[verticesCount];
+        normals = new Vector3[verticesCount];
+        tris = new int[gridPerMeshSide * gridPerMeshSide * 3 * 2];
+        //uvs = new Vector2[verticesCount];
+
+        mf.mesh = mesh;
+    }
+
+    void UpdateMeshInner(bool forceUpdate = false)
+    {
+        UpdateVerticesAndNormals();
+        if (forceUpdate)
+        {
+            UpdateTris();
+            UpdateUVs();
+        }
+        
+    }
+
+    void UpdateVerticesAndNormals()
+    {
+        float unitLength = 1;
+        var maxLength = gridPerMeshSide * unitLength;
+        var verticesCount = (gridPerMeshSide + 1) * (gridPerMeshSide + 1);
+
+        for (int z = 0; z <= gridPerMeshSide; z++)
+        {
+            for (int x = 0; x <= gridPerMeshSide; x++)
+            {
+                int i = z * (gridPerMeshSide + 1) + x;
+                var zRela = (float)z * unitLength;
+                var xRela = (float)x * unitLength;
+                var zCood = zRela + transform.position.z;
+                var xCood = xRela + transform.position.x;
+                                              
+                float y0 = GetY(zCood, xCood);
+                vertices[i] = new Vector3(xRela, y0, zRela);
+
+                // used the cross product of partial derivatives to compute the normal
+                var step = 0.01f;
+                var y1 = GetY(zCood + step, xCood);
+                var tanZ = new Vector3(0, y1 - y0, step);
+
+                var y2 = GetY(zCood, xCood + step);
+                var tanX = new Vector3(step, y2 - y0, 0);
+
+                normals[i] = Vector3.Cross(tanZ, tanX).normalized;
+
+
+                //uvs[i] = new Vector2((float)x / n, (float)z / n);
+            }
+        }
+    }
+
+    float GetY(float z, float x)
+    {
+        float unitLength = 1;
+        var maxLength = gridPerMeshSide * unitLength;
+        return Perlin.Noise(z / maxLength, x / maxLength) * heightFactor;
+    }
+
+    void UpdateTris()
+    {
+        int triIndex = 0;
+        for (int z = 0; z < gridPerMeshSide; z++)
+        {
+            for (int x = 0; x < gridPerMeshSide; x++)
+            {
+                int leftBottom = z * (gridPerMeshSide + 1) + x;
+                int rightBottom = z * (gridPerMeshSide + 1) + x + 1;
+                int leftTop = (z + 1) * (gridPerMeshSide + 1) + x;
+                int rightTop = (z + 1) * (gridPerMeshSide + 1) + x + 1;
+
+                tris[triIndex++] = leftBottom;
+                tris[triIndex++] = leftTop;
+                tris[triIndex++] = rightBottom;
+                tris[triIndex++] = leftTop;
+                tris[triIndex++] = rightTop;
+                tris[triIndex++] = rightBottom;
+            }
+        }
+    }
+
+    void UpdateUVs()
+    {
+
+    }
+
+
+    private void ApplyMeshData()
+    {
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+        mesh.normals = normals;
+
+        // mesh.RecalculateNormals();                  
+    }
+}
---
+++ Week5/Assets/Scenes/Assignment/TerrainManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour {
+
+    #region vars
+
+    public Transform focus;
+    Vector3 lastFocusPoint;
+    
+    
+    public int gridEachMesh = 8;
+    public int gridLength = 1;
+
+    Vector3[] gizmoPositions = new Vector3[4];
+    HashSet<int> posiSet = new HashSet<int>();
+    
+    // the buffer of the meshes
+    Queue<GameObject> meshQueue = new Queue<GameObject>();
+
+    // a hashset to judge if we have already drawn a mesh
+    HashSet<Vector3> taken = new HashSet<Vector3>();
+
+    int firstMeshIndex = 0;
+    int currMeshIndex = 0;
+
+    public GameObject meshRoot;
+    public GameObject meshPrefab;
+    public int bufferSize = 6;
+
+    #endregion
+
+    private void Awake()
+    {  
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        focus = transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateMeshes(false);
+	}
+
+
+    void UpdateMeshes(bool forceUpdate)
+    {
+        var currIndexPosi = GetIndexPosition(focus.position);
+        if (forceUpdate || currIndexPosi != lastFocusPoint)
+        {
+            UpdateMeshesInner();
+        }
+        lastFocusPoint = currIndexPosi;
+
+        Debug.DrawLine(gizmoPositions[0], gizmoPositions[1]);
+        Debug.DrawLine(gizmoPositions[1], gizmoPositions[2]);
+        Debug.DrawLine(gizmoPositions[2], gizmoPositions[3]);
+        Debug.DrawLine(gizmoPositions[3], gizmoPositions[0]);
+    }
+
+  
+
+    void UpdateMeshesInner()
+    {
+        Vector3 flooredCoord = GetIndexPosition(focus.position);
+        float meshLength = gridEachMesh * gridLength;
+        gizmoPositions[0] = (flooredCoord + new Vector3(0, 0, 0)) * meshLength;
+        gizmoPositions[1] = (flooredCoord + new Vector3(0, 0, 1)) * meshLength;
+        gizmoPositions[2] = (flooredCoord + new Vector3(1, 0, 1)) * meshLength;
+        gizmoPositions[3] = (flooredCoord + new Vector3(1, 0, 0)) * meshLength;
+
+
+        var newPosi = GetWorldPositionFromIndex(flooredCoord);
+        if (taken.Contains(newPosi))
+            return;
+
+        if(meshQueue.Count != bufferSize) 
+        {         
+            var go = Instantiate(meshPrefab, newPosi, Quaternion.identity, meshRoot.transform);
+            meshQueue.Enqueue(go);
+            taken.Add(newPosi);
+        }
+        else
+        {
+            var firstGo = meshQueue.Dequeue();
+            taken.Remove(firstGo.transform.position);
+            firstGo.transform.position = newPosi;
+            meshQueue.Enqueue(firstGo);            
+        }
+    }
+
+
+    Vector3 GetWorldPositionFromIndex(Vector3 index)
+    {
+        float meshLength = gridEachMesh * gridLength;
+        return index * meshLength;
+    }
+
+    Vector3 GetIndexPosition(Vector3 posi)
+    {
+        float meshLength = gridEachMesh * gridLength;
+        Vector3 coord = posi / meshLength;
+        Vector3 flooredCoord = new Vector3(Mathf.Floor(coord.x), 0, Mathf.Floor(coord.z));
+
+        return flooredCoord;
+    }
+}
